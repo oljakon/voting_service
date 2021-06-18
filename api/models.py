04bffb67348c60beef6005774e68ae83b9ab9268
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-06-19 00:38:28 +0300

    create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class PollChoice(models.Model):
+    name = models.CharField(max_length=50)
+    vote_count = models.IntegerField(default=0)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
--- voting_service/settings.py
+++ voting_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'rest_framework'
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'voting_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'voting_service',
+        'USER': 'postgres',
+        'PASSWORD': '12',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
