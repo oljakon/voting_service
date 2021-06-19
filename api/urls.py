72ca10963c80cb646bd6518c76ddc99ac7b33aab
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-06-20 01:59:19 +0300

    add voting in poll

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreatePoll
+from .views import CreatePoll, VotePoll
 
 urlpatterns = [
-    path('createPoll/', CreatePoll.as_view())
+    path('createPoll/', CreatePoll.as_view()),
+    path('poll/', VotePoll.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.db.models import F
 
+from .models import PollChoice
 from .serializers import PollSerializer, PollChoiceSerializer
 
 
@@ -18,3 +20,20 @@ class CreatePoll(APIView):
                     choice.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VotePoll(APIView):
+    def post(self, request):
+        poll_id = request.data.get('poll_id')
+        choice_id = request.data.get('choice_id')
+
+        if poll_id and choice_id:
+            poll_choice = PollChoice.objects.filter(id=choice_id, poll=poll_id)
+
+            if poll_choice.exists():
+                poll_choice.update(vote_count=F('vote_count') + 1)
+                return Response(status.HTTP_200_OK)
+
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
